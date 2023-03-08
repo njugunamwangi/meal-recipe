6fc92109d12d471a9875c293fe5522b77dbfcc6e
njugunamwangi <njugunamwangi.n@gmail.com>
2023-03-08 05:21:39 +0300

    project setup

---
+++ README.md
@@ -0,0 +1,1 @@
+Vue meals recipe search
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <input type="text">
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import store from '../store';
+
+
+  const meals = computed(() => store.state.meals)
+</script>
